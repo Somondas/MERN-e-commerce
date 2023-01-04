74b72624d3d782de8cf4a183c8d4796fcda46f7f
Somon Das <somondas067@gmail.com>
2023-01-04 01:48:14 -0800

    Made two new functions, getProductReviews and deleteReview.

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getAllProducts,createProduct, updateProduct, deleteProduct,getProductDetails, createProductReview} = require("../controller/productController")
+const {getAllProducts,createProduct, updateProduct, deleteProduct,getProductDetails, createProductReview, getProductReviews, deleteReview} = require("../controller/productController")
 const {isAuthenticatedUser, authorizedUser} = require("../middleware/auth");
 // |                                                                                      
 
@@ -9,5 +9,6 @@ router.route("/products").get( getAllProducts);
 router.route("/products/new").post( isAuthenticatedUser,authorizedUser("admin"), createProduct);
 router.route("/products/:id").put(isAuthenticatedUser,authorizedUser("admin"), updateProduct).delete(isAuthenticatedUser,authorizedUser("admin"), deleteProduct).get(getProductDetails)
 router.route("/review").put(isAuthenticatedUser,createProductReview)
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser, deleteReview)
 
 module.exports = router;
