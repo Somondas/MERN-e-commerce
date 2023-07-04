8c7efa4002bf0fdd4d3660764ac5c49fadafed83
Somon Das <somondas067@gmail.com>
2023-07-04 21:17:32 +0530

    Made the loader. working on homepage

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import "./Home.css";
 import Product from "./Product.js";
 import MetaData from '../layouts/MetaData';
 import { getProduct } from "../../actions/productAction";
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-
+import Loader from '../layouts/Loader/Loader';
+import { useAlert } from 'react-alert';
 const product = {
     name: "Blue TShirt",
     images: [{ url: "https://static.cilory.com/601840-thickbox_default/royal-blue-full-sleeves-t-shirt-by-grunt.jpg" }],
@@ -14,40 +15,41 @@ const product = {
 
 }
 const Home = () => {
+    const alert = useAlert();
     const dispatch = useDispatch();
-
+    const { loading, error, products, productsCount } = useSelector((state) => state.products);
     useEffect(() => {
+        if (error) {
+            return alert.error(error);
+        }
         dispatch(getProduct());
 
-    }, [dispatch])
+    }, [dispatch, error])
 
 
     return (
         <>
-            <MetaData title="Ecommerce" />
-            <div className="banner">
-                <p>Welcome to Ecommerce</p>
-                <h1>FIND AMAZING PRODUCTS</h1>
-                <a href="#container">
-                    <button>
-                        Scroll
-                    </button>
-                </a>
-            </div>
-            <h2 className='homeHeading'>Featured Products</h2>
-            <div className="container" id='container'>
-
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-            </div>
-
+            {loading ? <Loader />
+                :
+                <Fragment>
+                    <MetaData title="Ecommerce" />
+                    <div className="banner">
+                        <p>Welcome to Ecommerce</p>
+                        <h1>FIND AMAZING PRODUCTS</h1>
+                        <a href="#container">
+                            <button>
+                                Scroll
+                            </button>
+                        </a>
+                    </div>
+                    <h2 className='homeHeading'>Featured Products</h2>
+                    <div className="container" id='container'>
+                        {products && products.map((product, key) => (
+                            <Product product={product} key={key} />
+                        ))}
+                    </div>
+                </Fragment>
+            }
 
 
 
