22b8a59bc046074ded514bd4d67004408ceae90f
Somon Das <somondas067@gmail.com>
2023-07-15 15:38:27 +0530

    Having issues with ReactNavbar. Working on Products page

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { Fragment, useEffect } from 'react';
+import "./Products.css";
+import { useDispatch, useSelector } from 'react-redux';
+import { clearError, getProduct, getProductDetails } from '../../actions/productAction';
+import { useParams } from 'react-router-dom';
+import ReactStars from "react-rating-stars-component";
+import Loader from "../layouts/Loader/Loader";
+import { useAlert } from "react-alert"
+import ProductCard from '../Home/ProductCard';
 
 function Products() {
+
+    const dispatch = useDispatch();
+    const { products, loading, error, productsCount } = useSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(getProduct())
+    }, [dispatch])
+
     return (
-        <div>Products</div>
+        <>
+            {
+                loading ? <Loader /> :
+                    <>
+                        <div className="productsHeading">Products</div>
+                        <div className="products">
+                            {
+                                products &&
+                                products.map((product) => (
+                                    <ProductCard key={product._id} product={product} />
+                                ))
+                            }
+                        </div>
+                    </>
+            }
+        </>
     )
 }
 
--- frontend/src/components/layouts/Header.js
+++ frontend/src/components/layouts/Header.js
@@ -24,6 +24,9 @@ const options = {
     nav2justifyContent: "flex-end",
     nav3justifyContent: "flex-start",
     nav4justifyContent: "flex-start",
+    profileIcon: true,
+    searchIcon: true,
+    cartIcon: true,
     link1ColorHover: "#eb4034",
     link1Margin: "1vmax",
     profileIconUrl: "/login",
@@ -41,7 +44,39 @@ const options = {
 
 function Header() {
     return (
-        <ReactNavbar {...options} />
+        <ReactNavbar
+            burgerColorHover="#eb4034"
+            logo={logo}
+            logoWidth="20vmax"
+            navColor1="white"
+            logoHoverSize="10px"
+            logoHoverColor="#eb4034"
+            link1Text="Home"
+            link2Text="Products"
+            link3Text="Contact"
+            link4Text="About"
+            link1Url="/"
+            link2Url="/products"
+            link3Url="/contact"
+            link4Url="/about"
+            link1Size="1.3vmax"
+            link1Color="rgba(35, 35, 35,0.8)"
+            nav1justifyContent="flex-end"
+            nav2justifyContent="flex-end"
+            nav3justifyContent="flex-start"
+            nav4justifyContent="flex-start"
+            link1ColorHover="#eb4034"
+            link1Margin="1vmax"
+            profileIcon={true}
+            profileIconUrl="/login"
+            profileIconColor="rgba(35, 35, 35,0.8)"
+            searchIconColor="rgba(35, 35, 35,0.8)"
+            cartIconColor="rgba(35, 35, 35,0.8)"
+            profileIconColorHover="#eb4034"
+            searchIconColorHover="#eb4034"
+            cartIconColorHover="#eb4034"
+            cartIconMargin="1vmax"
+        />
     )
 }
 
