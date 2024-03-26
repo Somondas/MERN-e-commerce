a4b8732b6eb5f3f1bafa45d567251f334de2b787
Somondas <somondas067@gmail.com>
2024-03-26 22:01:31 +0530

    Working on Product page

--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -1,56 +1,68 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import "./Products.css";
-import { useDispatch, useSelector } from 'react-redux';
-import { clearError, getProduct, getProductDetails } from '../../actions/productAction';
-import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  clearError,
+  getProduct,
+  getProductDetails,
+} from "../../actions/productAction";
+import { useParams } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import Loader from "../layouts/Loader/Loader";
-import { useAlert } from "react-alert"
-import ProductCard from '../Home/ProductCard';
+import { useAlert } from "react-alert";
+import ProductCard from "../Home/ProductCard";
 import Pagination from "react-js-pagination";
 
-function Products() {
-    const params = useParams();
-    const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1);
+const Products = ({ match }) => {
+  const params = useParams();
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const { products, loading, error, productsCount, resultPerPage } = useSelector(state => state.products);
-    const keyword = params.keyword;
+  const { products, loading, error, productsCount, resultPerPage } =
+    useSelector((state) => state.products);
+  const keyword = match.params.keyword;
 
-    const setCurrentPageNo = (e) => {
-        setCurrentPage(e)
-    }
-    useEffect(() => {
-        dispatch(getProduct(keyword))
-    }, [dispatch, keyword])
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
+  useEffect(() => {
+    dispatch(getProduct(keyword));
+  }, [dispatch, keyword]);
 
-    return (
-        <>
-            {
-                loading ? <Loader /> :
-                    <>
-                        <div className="productsHeading">Products</div>
-                        <div className="products">
-                            {
-                                products &&
-                                products.map((product) => (
-                                    <ProductCard key={product._id} product={product} />
-                                ))
-                            }
-                        </div>
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <div className="productsHeading">Products</div>
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
 
-                        <div className="paginationBox">
-                            <Pagination
-                                activePage={currentPage}
-                                itemsCountPerPage={resultPerPage}
-                                totalItemsCount={productsCount}
-                                onChage={setCurrentPageNo}
-                            />
-                        </div>
-                    </>
-            }
-        </>
-    )
-}
+          <div className="paginationBox">
+            <Pagination
+              activePage={currentPage}
+              itemsCountPerPage={resultPerPage}
+              totalItemsCount={productsCount}
+              onChange={setCurrentPageNo}
+              nextPageText="Next"
+              prevPageText="Prev"
+              firstPageText="1st"
+              lastPageText="Last"
+              itemClass="page-item"
+              linkClass="page-link"
+              activeClass="pageItemActive"
+              activeLinkClass="pageLinkActive"
+            />
+          </div>
+        </Fragment>
+      )}
+    </>
+  );
+};
 
-export default Products
+export default Products;
